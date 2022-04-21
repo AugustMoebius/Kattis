7673fe1a94f59baf24e72481c32d2c098ee75752
August Moebius <august.moebius@keylane.com>
2022-04-21 23:23:31 +0200

    union-find first attempt, time limit exceed

---
+++ tasks/unionfind/unionfind.py
@@ -0,0 +1,52 @@
+from sys import stdin
+
+firstLine = stdin.readline().split(" ")
+n = int(firstLine[0])
+q = int(firstLine[1])
+
+sets = []
+setIdx = {}
+
+def parse_row():
+    line_split = stdin.readline().split(" ")
+    is_operation = line_split[0] == "="
+    return (is_operation, int(line_split[1]), int(line_split[2]))
+
+def find_idxs(x, y):
+    x_idx = -1
+    y_idx = -1
+    for idx, s in enumerate(sets):
+        if x in s:
+            x_idx = idx
+        if y in s:
+            y_idx = idx
+        if x_idx != -1 and y_idx != -1:
+            break
+    return x_idx, y_idx
+
+def update_sets(x, y):
+    x_idx, y_idx = find_idxs(x, y)
+    
+    if x_idx == -1 and y_idx == -1:
+        sets.append({x, y})
+    elif y_idx == -1:
+        sets[x_idx].add(y)
+    elif x_idx == -1:
+        sets[y_idx].add(x)
+    else:
+        sets[x_idx] = sets[x_idx].union(sets[y_idx])
+        del sets[y_idx]
+
+for _ in range(q):
+    row = parse_row()
+    x = row[1]
+    y = row[2]
+    if (row[0]): # Operation
+        update_sets(x, y)
+    else: # Question
+        x_idx, y_idx = find_idxs(x, y) 
+        if x == y or x_idx == y_idx and x_idx != -1:
+            print("yes")
+        else:
+            print("no")
+
